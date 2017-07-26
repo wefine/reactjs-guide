2c02dfaab73dabba970f06f5b17326edb742c382
wefine <wefine@aliyun.com>
2017-07-26 20:04:32 +0800

    react-redux-basic, introduce createStore, reducer, subscribe, dispatch

--- react-redux-basic/src/index.js
+++ react-redux-basic/src/index.js
@@ -1,4 +1,37 @@
-import React from 'react';
 import {createStore} from 'redux';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            state += action.payload;
+            break;
+        case "SUB":
+            state -= action.payload;
+            break;
+        default:
+            break;
+    }
+
+    return state;
+};
+
 const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log("updated state: " + store.getState())
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "SUB",
+    payload: 110
+});
