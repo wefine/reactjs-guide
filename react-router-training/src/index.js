63db8b29a67cde4c52e8f70f7362df15a7dbbd66
wefine <wefine@aliyun.com>
2017-08-09 17:39:20 +0800

    react-router, add notfound route

--- react-router-training/src/index.js
+++ react-router-training/src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom';
 import { hashHistory, Route, Router } from 'react-router'
 import App from './App';
 import './index.css';
+import About from './modules/About'
+import Repos from './modules/Repos'
+import NotFound from './modules/NotFound'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={App} />
+        <Route path="/repos" component={Repos} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={NotFound} />
     </Router>
     , document.getElementById('root'));
 
