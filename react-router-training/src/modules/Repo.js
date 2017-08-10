4a4fef5e2b91b9c53b5928cf279a5bb169668ed9
wefine <wefine@aliyun.com>
2017-08-10 15:13:45 +0800

    react-router, update react-router to v3.0.5

--- react-router-training/src/modules/Repo.js
+++ react-router-training/src/modules/Repo.js
@@ -3,10 +3,9 @@ import React from 'react';
 const Repo = (props) => {
     return (
         <div>
-            <h2>{props.params.repoName}</h2>
+            <h2>{props.params["repoName"]}</h2>
         </div>
     )
 };
-
 
 export default Repo;
