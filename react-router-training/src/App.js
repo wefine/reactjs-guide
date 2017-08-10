757cdc02e8a76a04171cf35b2af3dc280ae249e8
wefine <wefine@aliyun.com>
2017-08-10 11:38:24 +0800

    react-router, 09-index-links

--- react-router-training/src/App.js
+++ react-router-training/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import NavLink from './modules/NavLink';
 import './App.css';
+import NavLink from './modules/NavLink';
 
 const app = (props) => {
     return (
         <div>
             <h1>React Router Tutorial</h1>
             <ul>
-                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
                 <li><NavLink to="/about">About</NavLink></li>
                 <li><NavLink to="/repos">Repos</NavLink></li>
             </ul>
