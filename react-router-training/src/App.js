6c109f74f10f66d81a6b958cce81e14befc924c2
wefine <wefine@aliyun.com>
2017-08-10 10:21:10 +0800

    react-router, 05-active-links

--- react-router-training/src/App.js
+++ react-router-training/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router';
+import NavLink from './modules/NavLink'
+import './App.css';
 
 const app = (props) => {
     return (
         <div>
             <h1>React Router Tutorial</h1>
             <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/repos">Repos</Link></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/repos">Repos</NavLink></li>
             </ul>
 
             {/* 增加嵌套嵌套组件的引用 */}
