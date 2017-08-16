7cfffab2d5ebefca06d9b9ecc314f06fbd9e18c0
wefine <wefine@aliyun.com>
2017-08-16 23:12:44 +0800

    react-xterm, can work init

--- react-xterm/src/App.js
+++ react-xterm/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Terminal socketURL="ws://localhost:3000/terminals/">
+          <Terminal socketURL="ws://localhost:8080/api/v1/namespaces/default/pods/pod-example/exec?stdout=1&stdin=1&stderr=1&tty=1&container=hello-world&command=bash">
 
           </Terminal>
       </div>
--- react-xterm/src/Terminal.js
+++ react-xterm/src/Terminal.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { findDOMNode } from 'react-dom';
-import io from 'socket.io-client/dist/socket.io.js';
+
 import 'xterm/dist/addons/fit/fit.js';
 import 'xterm/dist/addons/fullscreen/fullscreen.js';
 import Xterm from 'xterm/dist/xterm.js';
+import 'xterm/dist/xterm.css';
+import './Terminal.css'
+
+function utf8_to_b64(str) {
+    return window.btoa(window.unescape(encodeURIComponent(str)));
+}
+
+function b64_to_utf8(str) {
+    return decodeURIComponent(window.escape(window.atob(str)));
+}
 
 export default class Terminal extends React.Component {
     static propTypes = {
@@ -17,8 +27,9 @@ export default class Terminal extends React.Component {
     };
 
     constructor(props) {
+        console.log('Terminal constructor');
         super(props);
-        this.term = new Xterm({cursorBlink: true});
+        this.term = new Xterm({ cursorBlink: true, cols: 80, rows: 24 });
         this.socket = this.createSocket(props.socketURL);
         this.handleResize = this.handleResize.bind(this);
         this.width = props.width;
@@ -27,43 +38,62 @@ export default class Terminal extends React.Component {
     }
 
     createSocket(path) {
-        const {onError, onClose, initialEmit, title} = this.props;
+        debugger;
+        const { onError, onClose, initialEmit, title } = this.props;
         const term = this.term;
-        const socket = io({path, reconnection: false, forceNew: true});
+        const ws = new window.WebSocket(path, 'base64.channel.k8s.io');
         this.isChange = false;
-        socket.on('connect', () => {
-            socket.emit(initialEmit[0], initialEmit[1]);
+
+        ws.onopen = () => {
+            console.log('socket open...');
+            ws.send('0');
             term.write(title);
-        });
-        socket.on('error', (err) => {
+        };
+
+        ws.onerror = (err) => {
+            console.log('socket error...');
             term.write(`\x1b[31mError:${err}\x1b[m\r\n`);
             onError && onError();
-        });
-        socket.on('disconnect', () => {
+        };
+        ws.onclose = () => {
+            console.log('socket disconnect...');
             if (!this.isChange) {
                 onClose && onClose();
             }
-        });
-        socket.on('data', (data) => {
-            term.write(data);
-        });
+        };
+        ws.onmessage = function (ev) {
+            var data = ev.data.slice(1);
+            switch (ev.data[0]) {
+                case '1':
+                case '2':
+                case '3':
+                    term.write(b64_to_utf8(data));
+                    break;
+            }
+        };
         term.on('data', (data) => {
-            socket.emit('data', data);
+            console.log('term.on \'data\'...');
+            if (ws && ws.readyState === 1)
+                ws.send('0' + utf8_to_b64(data));
         });
-        return socket;
+        return ws;
     }
 
     close() {
+        console.log('close ...');
         this.term.destroy();
         this.socket.close();
     }
 
     handleResize() {
+        console.log('handleResize ...');
         this.term.fit();
     }
 
     componentWillMount() {
-        const {onClose, socketURL} = this.props;
+        console.log('componentWillMount ...');
+
+        const { onClose, socketURL } = this.props;
         if (!socketURL) {
             onClose && onClose();
             this.close();
@@ -71,21 +101,27 @@ export default class Terminal extends React.Component {
     }
 
     componentDidMount() {
+        console.log('componentDidMount ...');
+
         const terminalContainer = findDOMNode(this);
-        const {term} = this;
-        term.open(terminalContainer);
+        const { term } = this;
+        term.open(terminalContainer, false);
         this.props.fullscreen && term.toggleFullscreen();
         this.handleResize();
         window.addEventListener('resize', this.handleResize);
     }
 
     componentWillUnmount() {
+        console.log('componentWillUnmount ...');
+
         this.close();
         window.removeEventListener('resize', this.handleResize);
     }
 
     componentWillReceiveProps(nextProps) {
-        const {socketURL, width, height} = this.props;
+        console.log('componentWillReceiveProps ...');
+
+        const { socketURL } = this.props;
         if (socketURL !== nextProps.socketURL) {
             this.isChange = true;
             this.term.reset();
@@ -103,7 +139,7 @@ export default class Terminal extends React.Component {
 }
 
 Terminal.defaultProps = {
-    title: '\x1b[32mWelcome to use cSphere online terminal!\x1b[m\r\n',
+    title: '\x1b[32mWelcome to use dashboard terminal!\x1b[m\r\n',
     initialEmit: ['auth', 'terminal,50,20'],
     fullscreen: false,
 };
