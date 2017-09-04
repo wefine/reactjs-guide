aabb18e47b80ddf1eee92ea9dbdb3e5b8e24d202
wefine <wefine@aliyun.com>
2017-09-04 18:16:13 +0800

    redux-saga-starter, add side effects

--- redux-saga/redux-saga-starter/src/index.js
+++ redux-saga/redux-saga-starter/src/index.js
@@ -5,23 +5,31 @@ import createSagaMiddleware from 'redux-saga';
 
 import Counter from './Counter';
 import reducer from './reducers';
-import { helloSaga } from './saga';
+import rootSaga  from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     reducer,
     applyMiddleware(sagaMiddleware)
 );
-sagaMiddleware.run(helloSaga);
+sagaMiddleware.run(rootSaga);
 
-const action = type => store.dispatch({ type });
+const action = type => {
+    console.log(type);
 
-ReactDOM.render(
-    <Counter
-        value={store.getState()}
-        onIncrementAsync={() => action('INCREMENT_ASYNC')}
-        onIncrement={() => action('INCREMENT')}
-        onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-);
+    store.dispatch({ type });
+};
 
+function render() {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrementAsync={() => action('INCREMENT_ASYNC')}
+            onIncrement={() => action('INCREMENT')}
+            onDecrement={() => action('DECREMENT')} />,
+        document.getElementById('root')
+    );
+}
+
+render();
+store.subscribe(render);
--- redux-saga/redux-saga-starter/src/saga.js
+++ redux-saga/redux-saga-starter/src/saga.js
@@ -1,3 +1,27 @@
+import { delay } from 'redux-saga';
+import { all, put, takeEvery } from 'redux-saga/effects';
+
 export function* helloSaga() {
-    console.log('Hello Sagas!')
+    yield;
+    console.log('Hello Sagas!');
+}
+
+export function* incrementAsync() {
+    console.log("incrementAsync 1");
+    yield delay(1000);
+    console.log("incrementAsync 2");
+    yield put({ type: 'INCREMENT' });
+    console.log("incrementAsync 3");
+}
+
+export function* watchIncrementAsync() {
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+}
+
+// 單一進入點，一次啟動所有 Saga
+export default function* rootSaga() {
+    yield all([
+        helloSaga(),
+        watchIncrementAsync()
+    ])
 }
